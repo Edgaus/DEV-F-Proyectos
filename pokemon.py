62370266cbb4d107a9ef526fd8b419d34b6a7360
Edgar Agustin Contreras <66283015+Edgaus@users.noreply.github.com>
2022-08-01 11:25:44 -0600

    FInalversion

--- pokemon.py
+++ pokemon.py
@@ -1,16 +1,13 @@
 from struct import pack
 from tkinter import *
+from turtle import bgcolor
 from PIL import ImageTk,Image
 import random as rm
 from matplotlib.pyplot import text
 from requests import delete
 from sympy import ask
 
-
-
 # Pokemo_game
-
-
 
 class Pokemon_game:
   dano_base = 10
@@ -192,11 +189,13 @@ Pokemon={
 #Creating a label
 root = Tk() 
 root.title( "Pokemon")
+root.config(bg="black")
+
 
 #Your frame
 
 global your_frame
-your_frame= LabelFrame(root, text="You", padx=50, pady=20 )
+your_frame= LabelFrame(root, text="You", padx=50, pady=20, font=("Terminal", 10), bg="black",fg="white" )
 your_frame.grid(row=0, column=0)
 
 #Your frame stats
@@ -205,7 +204,7 @@ your_frame.grid(row=0, column=0)
 r=IntVar()
 
 global your_stats
-your_stats= LabelFrame(root, text='Your stats', padx=50, pady=73)
+your_stats= LabelFrame(root, text='Your stats', padx=50, pady=73, font=("Terminal", 10), bg="black",fg="white")
 your_stats.grid(row=0, column=1)
 
 def text_stats(img_number):
@@ -218,10 +217,11 @@ def text_stats(img_number):
   return(text)
 
 
-text_box = Text(
+text_box = Text( 
     your_stats,
+    font=("Terminal", 10), bg="black",fg="white",
     height=20,
-    width=40
+    width=40, 
     )
 
 text_box.grid(row=0, column=0)
@@ -287,7 +287,7 @@ image_result=[
               [my_sad_Pik, my_sad_Char, my_sad_Bul, my_sad_Squ]
  ]
 
-my_label = Label(your_frame,image=my_img_Pik)
+my_label = Label(your_frame,image=my_img_Pik, font=("Terminal", 10), bg="black",fg="white")
 my_label.grid( row=0, column=0, columnspan=3 )
 your_number=1
 
@@ -304,14 +304,14 @@ def forward(img_number):
     my_label.grid_forget()
     your_number=img_number
 
-    my_label=Label(your_frame,image=image_list[img_number-1])
-    button_forward = Button(your_frame, text=">>", command=lambda: forward(img_number+1))
-    button_back = Button(your_frame, text="<<", command=lambda : back(img_number-1 ))
+    my_label=Label(your_frame,image=image_list[img_number-1],bg="black",fg="white")
+    button_forward = Button(your_frame, text=">>", command=lambda: forward(img_number+1), font=("Terminal", 10), bg="black",fg="white")
+    button_back = Button(your_frame, text="<<", command=lambda : back(img_number-1 ), font=("Terminal", 10), bg="black",fg="white")
     my_label.grid( row=0, column=0, columnspan=3 )
-    button_name=Button(your_frame, text="Choose",command=enemy_image)
+    button_name=Button(your_frame, text="Choose",command=enemy_image, font=("Terminal", 10), bg="black",fg="white")
 
     if img_number==len(image_list):
-        button_forward=Button(your_frame, text=">>", state=DISABLED)
+        button_forward=Button(your_frame, text=">>", state=DISABLED, font=("Terminal", 10), bg="black",fg="white")
 
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
@@ -320,6 +320,7 @@ def forward(img_number):
     
     text_box = Text(
     your_stats,
+    font=("Terminal", 10), bg="black",fg="white",
     height=20,
     width=40
     )
@@ -340,14 +341,14 @@ def back(img_number):
     my_label.grid_forget()
     your_number=img_number
 
-    my_label=Label(your_frame,image=image_list[img_number-1])
-    button_forward = Button(your_frame, text=">>", command=lambda: forward(img_number+1))
-    button_back = Button(your_frame, text="<<", command=lambda : back(img_number-1 ))
+    my_label=Label(your_frame,image=image_list[img_number-1],bg="black",fg="white")
+    button_forward = Button(your_frame, text=">>", command=lambda: forward(img_number+1), font=("Terminal", 10), bg="black",fg="white")
+    button_back = Button(your_frame, text="<<", command=lambda : back(img_number-1 ), font=("Terminal", 10), bg="black",fg="white")
     my_label.grid( row=0, column=0, columnspan=3 )
-    button_name=Button(your_frame, text="Choose", command=enemy_image)
+    button_name=Button(your_frame, text="Choose", command=enemy_image, font=("Terminal", 10), bg="black",fg="white")
     
     if img_number==1:
-        button_back=Button(your_frame, text="<<", state=DISABLED)
+        button_back=Button(your_frame, text="<<", state=DISABLED, font=("Terminal", 10), bg="black",fg="white")
 
 
     my_label.grid(row=0, column=0, columnspan=3)
@@ -357,6 +358,7 @@ def back(img_number):
 
     text_box = Text(
     your_stats,
+    font=("Terminal", 10), bg="black",fg="white",
     height=20,
     width=40
     )
@@ -367,13 +369,13 @@ def back(img_number):
 #Images from the enemy
 
 my_ask=ImageTk.PhotoImage(Image.open("DEV-F-Proyectos/Images/question.png"))
-
-enemy_frame= LabelFrame(root, text="Enemy", padx=50, pady=33 )
+ 
+enemy_frame= LabelFrame(root, text="Enemy", padx=50, pady=33, font=("Terminal", 10), bg="black",fg="white")
 enemy_frame.grid(row=0, column=3)
 
 global enemy_label
 
-enemy_label = Label(enemy_frame, image=my_ask)
+enemy_label = Label(enemy_frame, image=my_ask, font=("Terminal", 10), bg="black",fg="white")
 enemy_label.grid( row=0, column=3 )
   
 def enemy_image():
@@ -383,12 +385,12 @@ def enemy_image():
       enemy_number=rm.randrange(0,len(image_list))
       if enemy_number!=your_number-1:
         break
-    enemy_label.config(image=image_list[enemy_number]) 
+    enemy_label.config(image=image_list[enemy_number], bg="black") 
 
 
-    button_back = Button(your_frame, text="<<", command=back, state=DISABLED)
-    button_forward = Button(your_frame, text=">>", command=back, state=DISABLED)
-    button_fight.config( text="Let´s fight",command=activate_fight, state=NORMAL)
+    button_back = Button(your_frame, text="<<", command=back, state=DISABLED,font=("Terminal", 10), bg="black",fg="white")
+    button_forward = Button(your_frame, text=">>", command=back, state=DISABLED,font=("Terminal", 10), bg="black",fg="white")
+    button_fight.config( text="Let´s fight",command=activate_fight, state=NORMAL,font=("Terminal", 10), bg="black",fg="white")
     button_name.config(state=DISABLED)
 
     button_back.grid(row=1, column=0)
@@ -399,16 +401,16 @@ def enemy_image():
 
 #Botones iniciales
 
-button_back = Button(your_frame, text="<<", command=back, state=DISABLED)
-button_name=Button(your_frame, text="Choose", command=enemy_image)
-button_forward = Button(your_frame, text=">>", command= lambda: forward(2))
+button_back = Button(your_frame, text="<<", command=back, state=DISABLED, font=("Terminal", 10), bg="black",fg="white")
+button_name=Button(your_frame, text="Choose", command=enemy_image, font=("Terminal", 10), bg="black",fg="white")
+button_forward = Button(your_frame, text=">>", command= lambda: forward(2), font=("Terminal", 10), bg="black",fg="white")
 
 button_back.grid(row=1, column=0)
 button_name.grid(row=1, column=1)
 button_forward.grid(row=1, column=2)
 
 
-button_fight=Button(root, text="Lets fight",state=DISABLED)
+button_fight=Button(root, text="Lets fight",state=DISABLED, font=("Terminal", 10), bg="black",fg="white")
 button_fight.grid(row=5,column=1)
 
 
@@ -423,6 +425,7 @@ Peleadores=[Pikachu,charmander, Bulbasuar,Squirtle ]
 def text_func(text):
   text_box = Text(
     your_stats,
+    font=("Terminal", 10), bg="black",fg="white",
     height=20,
     width=40
     )
@@ -430,11 +433,7 @@ def text_func(text):
   text_box.insert('end', text)
   text_box.config(state='disabled')
 
-  button_fight.config( text="Siguiente turno: ",command=activate_fight)
-
-
-  
-
+  button_fight.config( text="Siguiente turno: ",command=activate_fight, font=("Terminal", 10), bg="black",fg="white")
 
 def delscreen():
   for child in your_stats.winfo_children():
@@ -449,7 +448,7 @@ def delscreenf(number):
     textf="Has perdido"
   text_func(textf)
   
-  button_fight.config(state=DISABLED)
+  button_fight.config(state=DISABLED,font=("Terminal", 10), bg="black",fg="white")
   
   final()
 
@@ -461,11 +460,11 @@ def fight():
 def botton():
   a=r.get()
   my_label.config(image=image_attack[your_number-1][r.get()-1])
-  button_fight.config(text="Confirmar ataque: "+str(a),command=fight)
+  button_fight.config(text="Confirmar ataque: "+str(a),command=fight, font=("Terminal", 10), bg="black",fg="white")
 
   
 def final():
-  Termina=Button(root,text="Terminar" ,command=root.quit).grid(row=3, column=1)
+  Termina=Button(root,text="Terminar" ,command=root.quit, font=("Terminal", 10), bg="black",fg="white").grid(row=3, column=1)
 
 def activate_fight():
 
@@ -475,15 +474,12 @@ def activate_fight():
    
   your_stats.config( text='Lets fight')
       
-  Radiobutton(your_stats, text=" 80% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][0][0], variable=r, value=1, command=botton).grid(row=3, column=0)
-  Radiobutton(your_stats, text=" 90% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][1][0], variable=r, value=2, command=botton).grid(row=4, column=0)
-  Radiobutton(your_stats, text=" 67% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][2][0], variable=r, value=3, command=botton).grid(row=5, column=0)
-  Radiobutton(your_stats, text=" 50% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][3][0], variable=r, value=4, command=botton).grid(row=6, column=0)  
+  Radiobutton(your_stats, text=" 80% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][0][0], variable=r, value=1, command=botton, font=("Terminal", 10), bg="black",fg="white").grid(row=3, column=0)
+  Radiobutton(your_stats, text=" 90% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][1][0], variable=r, value=2, command=botton, font=("Terminal", 10), bg="black",fg="white").grid(row=4, column=0)
+  Radiobutton(your_stats, text=" 67% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][2][0], variable=r, value=3, command=botton, font=("Terminal", 10), bg="black",fg="white").grid(row=5, column=0)
+  Radiobutton(your_stats, text=" 50% de exito: "+Pokemon[your_number-1]["ataques_por_esc"][3][0], variable=r, value=4, command=botton, font=("Terminal", 10), bg="black",fg="white",).grid(row=6, column=0)  
 
-  button_fight.config( text="Escoger ataque")
-  
-
-
+  button_fight.config( text="Escoger ataque") 
 
 def victoria(number):
   button_fight.config(text="Se acabo", command=delscreenf(number))
